e4cf8cd3f7cf6c30f96f365d96f6259219be10f2
Dmitriy Khvan <dimon.twin@gmail.com>
2022-06-02 15:57:24 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+declare var AJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements OnInit {
+  @ViewChild('datePicker', { static: true }) datePickerRef!: ElementRef;
+  file: any = [];
+
+  ngOnInit(): void {
+    console.log(this.datePickerRef.nativeElement);
+
+    AJS.$('#select2-example').auiSelect2();
+    // const demo1 = document.getElementById('demo-range-1');
+    const controller = new AJS.DatePicker(this.datePickerRef.nativeElement, {
+      overrideBrowserDefault: true,
+    });
+
+    AJS.$(function () {
+      AJS.$('.ffi input[type="file"]').fancyFileInput();
+    });
+
+    AJS.$('#simple-tooltip').tooltip({ gravity: 'e' });
+
+    AJS.$('#custom-tooltip').tooltip({
+      title: function () {
+        var index = AJS.$('a').index(this);
+        return 'This is link number ' + index + ' in all of the page';
+      },
+    });
+  }
+
+  upload(event: any) {
+    console.log(event.target.files);
+    this.file = event.target.files;
+  }
 }
