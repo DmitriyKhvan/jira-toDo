551687dd7b8fa37a70ea4f521cfcb49163197f68
Dmitriy Khvan <dimon.twin@gmail.com>
2022-06-03 17:13:31 +0500

    drag & drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 declare var AJS: any;
 
@@ -8,34 +8,29 @@ declare var AJS: any;
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  @ViewChild('datePicker', { static: true }) datePickerRef!: ElementRef;
-  file: any = [];
-
   ngOnInit(): void {
-    console.log(this.datePickerRef.nativeElement);
+    var sidebar = AJS.sidebar('.aui-sidebar');
 
-    AJS.$('#select2-example').auiSelect2();
-    // const demo1 = document.getElementById('demo-range-1');
-    const controller = new AJS.DatePicker(this.datePickerRef.nativeElement, {
-      overrideBrowserDefault: true,
+    // Try to expand the sidebar.
+    if (sidebar.isCollapsed()) {
+      sidebar.expand();
+    }
+
+    // Listen to collapse events.
+    sidebar.on('collapse-start', function () {
+      console.log('Sidebar is collapsing!');
     });
 
-    AJS.$(function () {
-      AJS.$('.ffi input[type="file"]').fancyFileInput();
+    sidebar.on('collapse-end', function () {
+      console.log('Sidebar is now collapsed!');
     });
 
-    AJS.$('#simple-tooltip').tooltip({ gravity: 'e' });
-
-    AJS.$('#custom-tooltip').tooltip({
-      title: function () {
-        var index = AJS.$('a').index(this);
-        return 'This is link number ' + index + ' in all of the page';
-      },
-    });
-  }
-
-  upload(event: any) {
-    console.log(event.target.files);
-    this.file = event.target.files;
+    // Event delegation for custom interactions.
+    // sidebar.$el.on('click', '.clone', function (e: any) {
+    //   if (sidebar.isCollapsed()) {
+    //     e.preventDefault();
+    //     cloneDialog.show();
+    //   }
+    // });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DragDropComponent } from './components/drag-drop/drag-drop.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, DragDropComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    DragDropModule,
+    HttpClientModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
